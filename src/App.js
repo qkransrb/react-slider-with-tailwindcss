2c95ca6de7fb7dcd409f24d3911f892c2e1cb284
qkransrb <qkransrb90@gmail.com>
2022-12-16 20:25:49 +0900

    react-slider-with-tailwindcss

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
+
+const slides = [
+  {
+    url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+  },
+];
+
+const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-5 -translate-x-0 translate-y-[-50%] text-white text-2xl rounded-full p-2 bg-black/20 cursor-pointer">
+        <BsChevronLeft size={30} onClick={prevSlide} />
+      </div>
+      <div className="hidden group-hover:block absolute top-[50%] right-5 -translate-x-100 translate-y-[-50%] text-white text-2xl rounded-full p-2 bg-black/20 cursor-pointer">
+        <BsChevronRight size={30} onClick={nextSlide} />
+      </div>
+
+      <div className="flex top-4 justify-center items-center py-2">
+        {slides.map((slide, index) => (
+          <div key={index} className="text-2xl cursor-pointer">
+            <RxDotFilled
+              onClick={() => goToSlide(index)}
+              className={
+                index === currentIndex ? "text-gray-900" : "text-gray-300"
+              }
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
